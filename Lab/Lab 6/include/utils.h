df6fa21233a3151ce553333ada0e55e98c1dd912
Didar Shvarsman <d@shvarsman.ru>
2025-06-20 10:17:18 +0300

    Create utils.h

---
+++ Lab/Lab 6/include/utils.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <vector>
+#include <string>
+
+bool isValidString(const std::string& str, char A, char B);
+long long multiplyLengths(const std::vector<std::string>& strings);
